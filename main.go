e613ee4ecfccb560b6b4f07409836ac6e85c1cc3
Keegan Campbell <kfcampbell@github.com>
2022-01-10 14:52:54 -0500

    Embed the Dockerfile (#4)
    * Initial commit of embedding the Dockerfile
    
    * Remove extraneous log

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"flag"
 	"fmt"
 	"log"
@@ -50,6 +51,10 @@ import (
 	"os/exec"
 	"strings"
 )
+
+// dockerfile contains the docker specification for our versioned dependencies
+//go:embed Dockerfile
+var dockerfile string
 
 func main() {
 	log.SetPrefix("proto-gen-go: ")
@@ -90,29 +95,3 @@ func main() {
 	}
 	log.Println("done")
 }
-
-// This Dockerfile produces an image that runs the protocol compiler
-// to generate Go declarations for messages and Twirp RPC interfaces.
-//
-// For build reproducibility, it is explicit about the versions of its
-// dependencies, which include:
-// - the golang base docker image (linux, go, git),
-// - protoc,
-// - Go packages (protoc-gen-go and protoc-gen-twirp),
-// - apt packages (unzip).
-const dockerfile = `
-FROM golang:1.16.5
-
-WORKDIR /work
-
-RUN apt-get update && \
-    apt-get install -y unzip=6.0-23+deb10u2 && \
-    curl --location --silent -o protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protoc-3.13.0-linux-x86_64.zip && \
-    unzip protoc.zip -d /usr/local/ && \
-    rm -fr protoc.zip
-
-RUN go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.20.0 \
-           github.com/twitchtv/twirp/protoc-gen-twirp@v5.12.1+incompatible
-
-ENTRYPOINT ["protoc"]
-`
